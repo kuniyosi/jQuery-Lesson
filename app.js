f21c2a97f48955a0672085a6db7445d405b39bf8
EC2 Default User <ec2-user@ip-172-31-19-237.ap-northeast-1.compute.internal>
2023-03-30 19:13:09 +0000

    [Finish]javascript7

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 $(function(){
-  $(".box1").slideDown(5000,function(){
-    $('.box1').css({
-      'background-color':'#0000ff',
-      'height':'200px',
-      'width':'100px'
-    }).slideUp();
+  $('button').on('click',function(){
+    $('ul').children().css('color','red');
   });
 });
-  
